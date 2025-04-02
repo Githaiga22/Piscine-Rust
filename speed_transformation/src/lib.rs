d28d7db9faa351f20acb80ef5df34b4404c54c27
allkamau <allangithaiga5@gmail.com>
2025-04-02 15:53:57 +0300

    refactor: update km/h to m/s conversion function for clarity and accuracy

--- speed_transformation/src/lib.rs
+++ speed_transformation/src/lib.rs
@@ -1,3 +1,6 @@
-pub fn km_per_hour_to_metres_per_second(km_h: f64) -> f64 {
-    km_h * (5.0 / 18.0) 
+// src/lib.rs
+
+/// Converts speed from km/h to m/s.
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    km_h * 1000.0 / 3600.0
 }
