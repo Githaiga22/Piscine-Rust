816c828d4c808bb424e1182ce92687481ee7bbfa
Githaiga22 <allangithaiga5@gmail.com>
2025-04-04 01:00:19 +0300

    Fix

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,100 +1,63 @@
 pub fn delete_and_backspace(s: &mut String) {
-	let mut i = 0;
-	let mut start : usize;
-	let mut chars : usize;
-
-	while i < s.len() {
-		// println!("{}", s.len());
-		start = i;
-		while i < s.len() && s.chars().nth(i).unwrap()== '-' {
-				i += 1;
-		}
-		chars = i - start;
-		if chars != 0 {
-			s.replace_range(i - 2 * chars..i, "");
-			i = i - 2 * chars;
-			// println!("- {} {}", s, i);
-		}
-
-		start = i;
-		while i < s.len() && s.chars().nth(i).unwrap()== '+' {
-				i += 1;
-		}
-		chars = i - start;
-		if chars != 0 {
-			// println!("{}", chars);
-			s.replace_range(start..start + 2 * chars, "");
-			i = i - 2 * chars;
-			// println!("{}", s);
-		}
-		i += 1;
-	}
+    let mut chars: Vec<char> = s.chars().collect();
+    let mut i = chars.len() ;	
+	while i  > 0 {
+		i -= 1;
+        if chars[i] == '+' {
+			chars.remove(i); 
+            if i < chars.len() {
+                chars.remove(i);
+            }
+        }
+    }	
+    i = 0;
+    while i < chars.len() {
+        if chars[i] == '-' {
+            if i > 0 {
+                chars.remove(i - 1);
+                i -= 1;
+            }
+            chars.remove(i); 
+        }  else {
+            i += 1;
+        }
+    }
+	
+    *s = chars.into_iter().collect();
 }
 
-pub fn is_correct(v: &mut Vec<&str>) -> usize {
-	let mut num1 : String = String::new();
-	let mut nums : Vec<i64> = Vec::new();
-	let v2 : Vec<&str> = v.clone();
-	let mut typ : i8 = -1;
-	let mut res : i64 = 1;
-	let mut corr = 0;
-	let l = v.len();
-	v.clear();
 
-	for w in v2 {
-		num1 = "".to_string();
-		nums.clear();
-		for i in w.chars() {
-			if i >= '0' && i <= '9' {
-				num1.push(i);
-			} else if i == '=' {
-				nums.push(num1.parse().unwrap());
-				num1 = "".to_string();
-			}
-			else if i == '-' {
-				nums.push(num1.parse().unwrap());
-				num1 = "".to_string();
-				typ = 0;
 
-			} else if i == '+' {
-				nums.push(num1.parse().unwrap());
-				num1 = "".to_string();
-				typ = 1;
-			} else if i == '*' {
-				nums.push(num1.parse().unwrap());
-				num1 = "".to_string();
-				typ = 2;
-			} else if i == '/' {
-				nums.push(num1.parse().unwrap());
-				num1 = "".to_string();
-				typ = 3;
-			} else {
-				nums.push(num1.parse().unwrap());
-				num1 = "".to_string();
-				typ = 4;
-			}
-		}
 
-		nums.push(num1.parse().unwrap());
-		if typ == 0 { 
-			res = nums[0] - nums[1];
-		} else if typ == 1 {
-			res = nums[0] + nums[1];
-		} else if typ == 2 {
-			res = nums[0] * nums[1];
-		} else if typ == 3 {
-			res = nums[0] / nums[1];
-		} else {
-			res = nums[0] % nums[1];
-		}
 
-		if res == nums[2] {
-			corr += 1;
-			v.push("✔");
-		} else {
-			v.push("✘");
-		}
-	}
 
-	return corr * 100 / l  as usize;
+pub fn do_operations(v: &mut Vec<String>) {
+    for element in v.iter_mut() {
+        let operator_index = element.chars().position(|c| c == '+' || c == '-');
+        if let Some(i) = operator_index {
+            let operator = element.chars().nth(i);
+            let (left, right) = element.split_at(i);
+            let x = left.trim().parse::<i32>().expect("invalid number");
+            let y = right.trim().parse::<i32>().expect("invalid number");
+            if let Some(o) = operator {
+                match o {
+                    '+' => {
+						*element = (x + y).to_string()
+					},
+                    '-' => {
+                        let y = -y;
+						*element = (x - y).to_string()
+					}
+					_ => println!("{} is not a valid operator!!",o)
+                }
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
