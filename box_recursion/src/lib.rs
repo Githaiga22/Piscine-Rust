1b561f0f9380627f32fc6c3ae24949bd0da64562
ALLAN ROBINSON <162472663+Githaiga22@users.noreply.github.com>
2025-08-11 11:50:04 +0300

    Update lib.rs
    fix: modify changes

--- box_recursion/src/lib.rs
+++ box_recursion/src/lib.rs
@@ -1,32 +1,58 @@
-#[derive(Debug)]
-pub struct WorkEnvironment {
-    pub grade: Link,
+#[derive(Debug, PartialEq, Clone)]
+pub enum Role {
+    CEO,
+    Manager,
+    Worker,
+}
+
+impl From<&str> for Role {
+    fn from(s: &str) -> Self {
+        match s.to_lowercase().as_str() {
+            "ceo" => Role::CEO,
+            "manager" => Role::Manager,
+            _ => Role::Worker, // default for "Normal Worker" and others
+        }
+    }
 }
 
 pub type Link = Option<Box<Worker>>;
 
 #[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+#[derive(Debug)]
 pub struct Worker {
-    pub role: String,
+    pub role: Role,
     pub name: String,
     pub next: Link,
 }
 
 impl WorkEnvironment {
-    pub fn new() -> WorkEnvironment { WorkEnvironment { grade: None } }
-
-    pub fn add_worker(&mut self, role: String, name: String) {
-        let new_worker = Box::new(Worker { role, name, next: self.grade.take() });
-        self.grade = Some(new_worker);
+    pub fn new() -> Self {
+        WorkEnvironment { grade: None }
     }
+
+    pub fn add_worker(&mut self, name: &str, role: &str) {
+        let new_worker = Worker {
+            role: Role::from(role),
+            name: name.to_string(),
+            next: self.grade.take(), // old head becomes next
+        };
+        self.grade = Some(Box::new(new_worker));
+    }
+
     pub fn remove_worker(&mut self) -> Option<String> {
         self.grade.take().map(|worker| {
-            self.grade = worker.next;
+            self.grade = worker.next; // new head is the next worker
             worker.name
         })
     }
-    pub fn last_worker(&self) -> Option<(String, String)> {
-        self.grade.as_ref().map(|worker| (worker.name.clone(), worker.role.clone()))
-    }
 
+    pub fn last_worker(&self) -> Option<(String, Role)> {
+        self.grade
+            .as_ref()
+            .map(|worker| (worker.name.clone(), worker.role.clone()))
+    }
 }
