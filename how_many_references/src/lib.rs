6443ff9c39353d6e68ea1d46ccb89a1b3e9ced98
ALLAN ROBINSON <162472663+Githaiga22@users.noreply.github.com>
2025-08-11 11:54:22 +0300

    Update lib.rs
    fix: modify function

--- how_many_references/src/lib.rs
+++ how_many_references/src/lib.rs
@@ -1,12 +1,12 @@
-pub use std::rc::Rc;
+use std::rc::Rc;
 
 pub struct Node {
     pub ref_list: Vec<Rc<String>>,
 }
 
 impl Node {
-    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
-        Node { ref_list: ref_list }
+    pub fn new(ref_list: Vec<Rc<String>>) -> Self {
+        Node { ref_list }
     }
 
     pub fn add_element(&mut self, element: Rc<String>) {
@@ -14,11 +14,11 @@ impl Node {
     }
 
     pub fn rm_all_ref(&mut self, element: Rc<String>) {
-        self.ref_list.retain(|e| !Rc::ptr_eq(e, &element));
+        self.ref_list
+            .retain(|e| !Rc::ptr_eq(e, &element)); // remove if same allocation
     }
 }
 
 pub fn how_many_references(ref_list: &Rc<String>) -> usize {
-    // Return the strong count without subtracting 1.
     Rc::strong_count(ref_list)
 }
